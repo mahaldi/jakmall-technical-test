4685b94e489e618d293fd6763f8cd83a42ab7ddb
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-09 21:25:45 +0700

    consume stepper componen, fix indent space

--- src/views/payment/index.vue
+++ src/views/payment/index.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="payment">
-        Payment
-        <div class="payment__box">
-            <PaymentSummary />
-        </div>
+  <div class="payment">
+    <div class="payment__box">
+      <Stepper />
+      <PaymentSummary />
     </div>
+  </div>
 </template>
 <script>
-import PaymentSummary from '@/components/summary'
+import PaymentSummary from "@/components/summary";
+import Stepper from "@/components/stepper";
+
 export default {
-    name: 'Payment',
-    components: {
-        PaymentSummary
-    }
-    // created(){
-    //     if(isNaN(parseInt(this.$route.query.step))){
-    //         window.location.replace('/payment?step=1')
-    //     }
-    // },
-}
+  name: "Payment",
+  components: {
+    PaymentSummary,
+    Stepper
+  }
+  // created(){
+  //     if(isNaN(parseInt(this.$route.query.step))){
+  //         window.location.replace('/payment?step=1')
+  //     }
+  // },
+};
 </script>
 <style lang="scss" src="./style.scss" scoped />
