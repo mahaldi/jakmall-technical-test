86d29b0ebdc1af2114495c681042761bcb5f7fc5
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-09 11:59:16 +0700

    payment view

---
+++ src/views/payment/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Payment
+    </div>
+</template>
+<script>
+export default {
+    name: 'Payment',
+    mounted(){
+        console.log(this.$route)
+    }
+}
+</script>
+<style lang="scss" src="./style.scss" scoped />
