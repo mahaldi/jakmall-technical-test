bde28b5441a94b277aaf305b11ec2ec9f156df56
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-09 17:33:00 +0700

    payment page

--- src/views/payment/index.vue
+++ src/views/payment/index.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>
+    <div class="payment">
         Payment
+        <div class="payment__box">
+            <PaymentSummary />
+        </div>
     </div>
 </template>
 <script>
+import PaymentSummary from '@/components/summary'
 export default {
     name: 'Payment',
-    mounted(){
-        console.log(this.$route)
+    components: {
+        PaymentSummary
     }
+    // created(){
+    //     if(isNaN(parseInt(this.$route.query.step))){
+    //         window.location.replace('/payment?step=1')
+    //     }
+    // },
 }
 </script>
 <style lang="scss" src="./style.scss" scoped />
