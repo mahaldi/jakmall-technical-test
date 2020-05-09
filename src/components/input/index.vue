9418d0041e7e04b52b887905cafeab5f1ffd7261
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-09 11:30:56 +0700

    mandatory component

---
+++ src/components/input/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="jakmall-input jakmall-input--error">
+        <input type="text"/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" src="./style.scss" scoped />
