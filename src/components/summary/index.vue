f1502aa0d29e949cbcd15ab8df906a3e5af8e598
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-09 17:32:34 +0700

    summary component styling

--- src/components/summary/index.vue
+++ src/components/summary/index.vue
@@ -2,28 +2,27 @@
   <div class="summary">
     <div class="summary__top">
       <p class="heading">Summary</p>
-      <p class="jakmall-text jakmall-text--inter-ui jakmall-text--opt6">10 items purchased</p>
+      <p class="jakmall-text jakmall-text--opt6">10 items purchased</p>
     </div>
-		<div class="summary__middle">
-			<div v-for="(item , idx) in 2"  class="summary__item" :key="idx">
-				<p class="jakmall-text jakmall-text--mgb2">Delivery estimation</p>
-				<p class="jakmall-text jakmall-text--ft16 jakmall-text--success">today by Go-send</p>
-			</div>
+		<div class="summary__middle"  v-for="(item , idx) in 2" :key="idx">
+      <div class="summary__divider"></div>
+      <p class="jakmall-text jakmall-text--mgb2">Delivery estimation</p>
+      <p class="jakmall-text jakmall-text--ft16 jakmall-text--medium jakmall-text--success">today by Go-send</p>
 		</div>
     <div class="summary__bottom">
       <div v-for="(item , idx) in 2" class="summary__item" :key="idx">
         <div class="summary__item-name">
-          <span>Cost of goods</span>
+          <span class="jakmall-text">Cost of goods</span>
         </div>
         <div class="summary__item-cost">
-          <span>500,000</span>
+          <span class="jakmall-text jakmall-text--bold">500,000</span>
         </div>
       </div>
       <div class="summary__total">
-        <span>Total</span>
-        <span>505,900</span>
+        <span class="summary__total-text heading">Total</span>
+        <span class="summary__total-cost heading">505,900</span>
       </div>
-      <div class="summary__button">Continue to Payment</div>
+      <button class="summary__button">Continue to Payment</button>
     </div>
   </div>
 </template>
