a8db51c81dfd8c91d9bccac6e1b4d6a8c6decce1
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-10 23:24:16 +0700

    delivery details

--- src/components/forms/delivery-details/index.vue
+++ src/components/forms/delivery-details/index.vue
@@ -1,30 +1,68 @@
 <template>
-    <Form ref="form" :model="model" :rules="rules">
-			<FormItem prop="email">
-				<Input id="email" label="Email" v-model="model.email"/>
-			</FormItem>
-			<FormItem prop="phone">
-				<Input id="phone" label="Phone Number" v-model="model.phone"/>
-			</FormItem>
-			<FormItem prop="dropshipName">
-				<Input id="dropshipName" label="Dropshipper Name" v-model="model.dropshipName" disabled/>
-			</FormItem>
-    </Form>
+	<div class="delivery-details">
+
+		<div class="delivery-details__left">
+			<HeadingForm heading="Delivery details"/>
+			<div class="delivery-details__form-left">
+				<Form ref="form" :model="model" :rules="rules">
+					<FormItem prop="email">
+						<Input id="email" label="Email" v-model="model.email"/>
+					</FormItem>
+					<FormItem prop="phone">
+						<Input id="phone" label="Phone Number" v-model="model.phone"/>
+					</FormItem>
+				</Form>
+			</div>
+		</div>
+
+		<div class="delivery-details__right">
+			<div class="delivery-details__checkbox">
+				<CheckBox v-model="isDropship" label="Send as Dropshipper" />
+			</div>
+			<div class="delivery-details__form-right">
+				<Form :model="modelDropShip" :rules="rulesDropShip">
+					<FormItem prop="dropshipName">
+						<Input id="dropshipName" label="Dropshipper Name" v-model="modelDropShip.dropshipName" :disabled="!isDropship"/>
+					</FormItem>
+					<FormItem prop="dropShipPhone">
+						<Input id="dropShipPhone" label="Dropshipper Phone Number" v-model="modelDropShip.dropShipPhone" :disabled="!isDropship"/>
+					</FormItem>
+				</Form>
+			</div>
+		</div>
+
+	</div>
 </template>
 <script>
 import Form from '@/components/forms'
 import FormItem from '@/components/forms/form-item'
 import Input from '@/components/input'
+import HeadingForm from '@/components/forms/heading-form'
+import CheckBox from '@/components/checkbox'
 
 export default {
 	data() {
+		let PhoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-/,/0-9]*$/g
 		return {
+			isDropship: false,
 			model: {
 				email: '',
-				phone: '',
-				dropshipName: ''
+				phone: ''
 			},
-
+			modelDropShip: {
+				dropshipName: '',
+				dropShipPhone: ''
+			},
+			rulesDropShip: {
+				dropshipName: [
+					{ required: false, trigger: 'change' }
+				],
+				dropshipPhone: [
+					{ required: false, trigger: 'change' },
+					{ pattern : PhoneRegex, trigger: 'change'},
+					{ min: 6, max: 20, trigger: 'change'}
+				]
+			},
 			rules: {
 				email: [
 					{ required: true, trigger: 'change' },
@@ -32,19 +70,25 @@ export default {
 				],
 				phone: [
 					{ required: true, trigger: 'change' },
-					{ pattern : /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-/,/0-9]*$/g, trigger: 'change'},
+					{ pattern : PhoneRegex, trigger: 'change'},
 					{ min: 6, max: 20, trigger: 'change'}
-				],
-				dropshipName: [
-					{ required: false, trigger: 'change' }
 				]
 			}
+		}
+	},
+	watch: {
+		isDropship(val) {
+			this.modelDropShip.dropshipName = ''
+			this.modelDropShip.dropShipPhone = ''
+			this.rulesDropShip.dropshipName[0].required = val
 		}
 	},
 	components: {
 		Form,
 		FormItem,
-		Input
+		Input,
+		HeadingForm,
+		CheckBox
 	}
 }
 </script>
