3cec419a70a7922392e50eeaeb4191be03c7430e
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-10 17:20:32 +0700

    custome form component

---
+++ src/components/forms/form-item.vue
@@ -0,0 +1,169 @@
+<template>
+  <div
+    class="jakmall-form-item"
+    :class="[{
+        'jakmall-form-item--error': validateState === 'error',
+        'jakmall-form-item--success': validateState === 'success'}]"
+  >
+    <div class="jakmall-form-item__content">
+      <slot></slot>
+      <div class="jakmall-form-item__error" v-if="validateState === 'error' && form.showMessage">
+        {{ validateMessage }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AsyncValidator from "async-validator";
+import Emitter from "@/utils/emitter";
+import Merge from "@/utils/merge";
+
+export default {
+  name: "jakmall-form-item",
+  componentName: "jakmall-form-item",
+  data() {
+    return {
+      validateState: "",
+      validateDisabled: false,
+      validateMessage: "",
+      validator: {}
+    };
+  },
+  inject: ["_form"],
+  mixins: [Emitter, Merge],
+  props: {
+    prop: String
+  },
+  computed: {
+    form() {
+      let parent = this.$parent;
+      let parentName = parent.$options.componentName;
+      while (parentName !== "jakmall-form") {
+        parent = parent.$parent;
+        parentName = parent.$options.componentName;
+      }
+      return parent;
+    },
+    fieldValue() {
+      const model = this.form.model;
+      if (!model || !this.prop) {
+        return;
+      }
+
+      let path = this.prop;
+      if (path.indexOf(":") !== -1) {
+        path = path.replace(/:/, ".");
+      }
+
+      return this.getPropByPath(model, path, true).r;
+    }
+  },
+  methods: {
+    validate(trigger, callback = () => {}) {
+      const rules = this.getFilteredRule(trigger);
+
+      if (!rules || rules.length === 0) {
+        callback();
+        return true;
+      }
+      this.validateState = "validating";
+      const descriptor = {};
+      if (rules && rules.length > 0) {
+        rules.forEach(rule => {
+          delete rule.trigger;
+        });
+      }
+      descriptor[this.prop] = rules;
+      const validator = new AsyncValidator(descriptor);
+      const model = {};
+      model[this.prop] = this.fieldValue;
+      validator.validate(
+        model,
+        { firstFields: true },
+        (errors, invalidFields) => {
+          this.validateState = !errors ? "success" : "error";
+          this.validateMessage = errors ? errors[0].message : "";
+
+          callback(this.validateMessage, invalidFields);
+          this._form &&
+            this._form.$emit(
+              "validate",
+              this.prop,
+              !errors,
+              this.validateMessage || null
+            );
+        }
+      );
+    },
+    getPropByPath(obj, path, strict) {
+      let tempObj = obj;
+      path = path.replace(/\[(\w+)\]/g, ".$1");
+      path = path.replace(/^\./, "");
+
+      let keyArr = path.split(".");
+      let i = 0;
+      for (let len = keyArr.length; i < len - 1; ++i) {
+        if (!tempObj && !strict) break;
+        let key = keyArr[i];
+        if (key in tempObj) {
+          tempObj = tempObj[key];
+        } else {
+          if (strict) {
+            throw new Error("please transfer a valid prop path to form item!");
+          }
+          break;
+        }
+      }
+      return {
+        u: tempObj,
+        m: keyArr[i],
+        r: tempObj ? tempObj[keyArr[i]] : null
+      };
+    },
+    getRules() {
+      let formRules = this.form.rules;
+      const selfRules = this.rules;
+			
+      const prop = this.getPropByPath(formRules, this.prop || "");
+      formRules = formRules ? prop.u[this.prop || ""] || prop.r : [];
+      return [].concat(selfRules || formRules || []);
+    },
+    getFilteredRule(trigger) {
+      const rules = this.getRules();
+
+      return rules
+        .filter(rule => {
+          if (!rule.trigger || trigger === "") return true;
+          if (Array.isArray(rule.trigger)) {
+            return rule.trigger.indexOf(trigger) > -1;
+          } else {
+            return rule.trigger === trigger;
+          }
+        })
+        .map(rule => this.objectAssign({}, rule));
+    },
+    onFieldBlur() {
+      this.validate("blur");
+    },
+    onFieldChange() {
+      this.validate("change");
+    },
+    addValidateEvents() {
+      const rules = this.getRules();
+      if (rules.length > 0) {
+        this.$on("jakmall-form-blur", this.onFieldBlur);
+        this.$on("jakmall-form-change", this.onFieldChange);
+      }
+    }
+  },
+  mounted() {
+    if (this.prop) {
+      this.dispatch("jakmall-form", "jakmall-form-addField", [this]);
+      this.addValidateEvents();
+    }
+  },
+  beforeDestroy() {
+    this.dispatch("jakmall-form", "jakmall-form-removeField", [this]);
+  }
+};
+</script>
---
+++ src/components/forms/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <form autocomplete="on" @submit.prevent="onSubmit">
+    <slot></slot>
+  </form>
+</template>
+<script>
+import Merge from "@/utils/merge";
+export default {
+  name: "jakmall-form",
+  componentName: "jakmall-form",
+  mixins: [Merge],
+  props: {
+    model: {
+      type: Object,
+      required: true
+    },
+    rules: {
+      type: Object,
+      required: true
+    },
+    showMessage: {
+      type: Boolean,
+      default: false
+    },
+    validateOnRuleChange: {
+      type: Boolean,
+      default: true
+    }
+  },
+  provide() {
+    return {
+      _form: this
+    };
+  },
+  data() {
+    return {
+      fields: []
+    };
+  },
+  created() {
+    this.initEventChild();
+  },
+  watch: {
+    rules() {
+      if (this.validateOnRuleChange) {
+        this.validate(() => {});
+      }
+    }
+  },
+  methods: {
+    initEventChild() {
+      this.$on("jakmall-form-addField", field => {
+        if (field) {
+          this.fields.push(field);
+        }
+      });
+      this.$on("jakmall-form-removeField", field => {
+        if (field.prop) {
+          this.fields.splice(this.fields.indexOf(field), 1);
+        }
+      });
+    },
+    onSubmit() {
+      this.$emit("submit");
+    },
+    validate(callback) {
+      if (!this.model) {
+        console.warn("[Form] model is required for validate to work!");
+        return;
+      }
+
+      let promise;
+      if (typeof callback !== "function" && window.Promise) {
+        promise = new window.Promise((resolve, reject) => {
+          callback = function(valid) {
+            if (valid) {
+              resolve(valid);
+            } else {
+              reject(valid);
+            }
+          };
+        });
+      }
+      let valid = true;
+      let count = 0;
+
+      if (this.fields.length === 0 && callback) {
+        callback(true);
+      }
+
+      let invalidFields = {};
+      this.fields.forEach(field => {
+        field.validate("", (message, field) => {
+          if (message) {
+            valid = false;
+          }
+          invalidFields = this.objectAssign({}, invalidFields, field);
+          if (
+            typeof callback === "function" &&
+            ++count === this.fields.length
+          ) {
+            callback(valid, invalidFields);
+          }
+        });
+      });
+      if (promise) {
+        return promise;
+      }
+    }
+  }
+};
+</script>
