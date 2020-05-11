1a870b89cf3b5cad0fea10507a639006f45040af
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-11 10:59:53 +0700

    category button component

---
+++ src/components/forms/payment/category-button.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="category-button" :class="{'category-button--active' : isActive}" @click="$emit('click')">
+		<div class="category-button__content">
+			<p class="jakmall-text jakmall-text--medium jakmall-text--blackOpt60 jakmall-text--ft-13 jakmall-text--mgb-5" 
+				:class="{'jakmall-text--blackOpt80' : isActive}"> {{ firstText }} 
+			</p>
+			<p class="jakmall-text jakmall-text--bold jakmall-text--bluis-blackOpt60 jakmall-text--ft16 jakmall-text--mg0" 
+				:class="{'jakmall-text--bluis-black' : isActive}"> {{ secondText }} 
+			</p>
+		</div>
+		<i class="category-button__icon icon-oke" :class="{'category-button__icon--active' : isActive}"></i>
+	</div>
+</template>
+<script>
+export default {
+  props: {
+		isActive: {
+			default: false,
+			type: Boolean
+		},
+		firstText: {
+			default: 'GO-SEND',
+			type: String
+		},
+		secondText: {
+			default: '15,000',
+			type: String
+		}
+	}
+}
+</script>
