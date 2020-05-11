5eff9a9a3488c48ff94391e113093b3352ab8660
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-11 11:01:05 +0700

    consume payment form, and state form

--- src/views/payment/index.vue
+++ src/views/payment/index.vue
@@ -5,15 +5,16 @@
 				<Stepper />
 			</div>
 			<div class="payment__form-section">
-				<div class="payment__back">
+				<div class="payment__back" @click="step -= 1">
           <i class="payment__back-icon icon-left"></i>
           <p class="jakmall-text jakmall-text--medium jakmall-text--blackOpt60">Back to cart</p>
         </div>
-        <DeliveryDetails />
+        <DeliveryDetails v-show="step === 1"/>
+        <PaymentForm v-show="step === 2"/>
 			</div>
       <div class="payment__divider"></div>
 			<div class="payment__summary-section">
-				<PaymentSummary />
+				<PaymentSummary @submit="onSubmit"/>
 			</div>
     </div>
   </div>
@@ -22,19 +23,26 @@
 import PaymentSummary from "@/components/summary";
 import Stepper from "@/components/stepper";
 import DeliveryDetails from '@/components/forms/delivery-details'
+import PaymentForm from '@/components/forms/payment'
 
 export default {
   name: "Payment",
   components: {
     PaymentSummary,
     Stepper,
-    DeliveryDetails
+    DeliveryDetails,
+    PaymentForm
+  },
+  data() {
+    return {
+      step: 1
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.step += 1
+    }
   }
-  // created(){
-  //     if(isNaN(parseInt(this.$route.query.step))){
-  //         window.location.replace('/payment?step=1')
-  //     }
-  // },
 };
 </script>
 <style lang="scss" src="./style.scss" scoped />
