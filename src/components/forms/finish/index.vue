2d58679492ce483274b2797b12fe273deb79d50b
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-11 23:58:58 +0700

    finish state

---
+++ src/components/forms/finish/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="payment-finish">
+		<div class="payment-finish__heading">
+			<HeadingForm heading="Thank you"/>
+		</div>
+		<div class="payment-finish__content">
+			<p class="jakmall-text">Order ID : XXKYB</p>
+			<p class="jakmall-text jakmall-text--blackOpt60">Your order will be delivered today with GO-SEND</p>
+		</div>
+		<div class="payment-finish__back">
+			<BackFormButton />
+		</div>
+	</div>
+</template>
+<script>
+import HeadingForm from '@/components/forms/heading-form'
+import BackFormButton from '@/components/buttons/back-form'
+
+export default {
+  components: {
+		HeadingForm,
+		BackFormButton
+	}
+}
+</script>
+<style lang="scss" src="./style.scss" scoped />
