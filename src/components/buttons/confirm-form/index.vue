2146fb3419bf458e8f04d075f95339b4135449c5
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-12 13:59:34 +0700

    confirm button

---
+++ src/components/buttons/confirm-form/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="confirm-button" @click="$emit('click')">{{ label }}</button>
+</template>
+<script>
+export default {
+    props: {
+        label: {
+            default: ''
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.confirm-button {
+
+    outline: none;
+    cursor: pointer;
+    height: 60px;
+    color: $white;
+    background-color: $secondary;
+    width: 100%;
+    font-size: 18px;
+    font-weight: 500;
+    box-shadow: 3px 5px 10px 0px $secondaryOpt20;
+}
+</style>
