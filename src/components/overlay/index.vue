32b7038e746af05e3d8d169fb90380a511809a29
mahaldiWS_ <aldi@umroh.com>
2020-05-12 17:28:51 +0700

    overlay commponent

---
+++ src/components/overlay/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="jakmall-overlay" :class="{ 'jakmall-overlay__full' : isOverlayFull }" @click.stop="$emit('click')"></div>
+</template>
+<script>
+export default {
+    props : {
+        isOverlayFull : {
+            type : Boolean,
+            default : false
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.jakmall-overlay{
+    $self: &;
+    background: #000000;
+    opacity: 0.8;
+    width: 100%;
+    height: 100%;
+    z-index: 997;
+    padding: 20px;
+    position: fixed;
+    top: 8%;
+    &__full{
+        cursor: pointer;
+        top: 0;
+        z-index: 2000;
+        padding: 0;
+    }
+}
+</style>
