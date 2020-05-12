7da339e4615fb8253a551a0cf3d2fc02c7302642
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-12 14:38:08 +0700

    webpack configure

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-	// options...
+	configureWebpack: {
+		optimization: {
+		  runtimeChunk: 'single',
+		  splitChunks: {
+			chunks: 'all',
+			maxInitialRequests: Infinity,
+			minSize: 0,
+			cacheGroups: {
+			  vendor: {
+				test: /[\\/]node_modules[\\/]/,
+				name(module) {
+				  const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+				  return `npm.${packageName.replace('@', '')}`;
+				},
+			  },
+			},
+		  },
+		},
+	},
 	css: {
 		loaderOptions: {
 			sass: {
