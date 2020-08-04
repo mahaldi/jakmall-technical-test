b3ffba400a34f9becf468b187b78038431746bda
mahaldiWS <aldiwijayasaputra@gmail.com>
2020-08-04 14:02:05 +0700

    testing hotfix

--- src/components/forms/heading-form/index.vue
+++ src/components/forms/heading-form/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="payment-heading">
-    <h3 class="payment-heading__tag heading heading--large">{{ heading }}</h3>
+    <h3 class="payment-heading__tag heading heading--large">{{ heading }} hotfix 1.0.1</h3>
 		<div class="payment-heading__shadow"></div>
   </div>
 </template>
